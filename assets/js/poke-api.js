65942da67af631c982fd0b77650d841d56992c0a
justin-rignault <justin.rignault@gmail.com>
2022-12-26 20:01:37 -0300

    change promise chaining to async/await

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -17,19 +17,18 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
     return pokemon
 }
 
-pokeApi.getPokemonDetail = (pokemon) => {
-   return fetch(pokemon.url)
-            .then((response) => response.json())
-            .then(convertPokeApiDetailToPokemon)
+pokeApi.getPokemonDetail = async (pokemon) => {
+    const response = await fetch(pokemon.url)
+    const pokemons = await response.json()
+    return convertPokeApiDetailToPokemon(pokemons)
 }
 
-pokeApi.getPokemons = (offset = 0, limit = 5) => {
+pokeApi.getPokemons = async (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-    return fetch(url)
-        .then((response) => response.json())
-        .then((jsonBody) => jsonBody.results)
-        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
-        .then((detailRequests) => Promise.all(detailRequests))
-        .then((pokemonsDetails) => pokemonsDetails)
+    const response = await fetch(url)
+    const jsonBody = await response.json()
+    const pokemons = await jsonBody.results.map(pokeApi.getPokemonDetail)
+    const pokemonDetails = await Promise.all(pokemons)
+    return pokemonDetails
 }
